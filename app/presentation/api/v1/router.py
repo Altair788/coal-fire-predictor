53523d45c2a4c6a8b0a364d5bf123dfa64e1dff2
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-11-23 00:45:16 +0300

    feat(endpoints.router): add api/v1 router

---
+++ app/presentation/api/v1/router.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+from .endpoints import data, predict, dashboard, analytics, pile_history
+
+router = APIRouter(prefix="/api/v1")
+
+router.include_router(data.router, tags=["Data"])
+router.include_router(predict.router, prefix="/predict", tags=["Prediction"])
+router.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
+router.include_router(analytics.router, prefix="/analytics", tags=["Analytics"])
+router.include_router(pile_history.router, prefix="", tags=["Pile History"])
