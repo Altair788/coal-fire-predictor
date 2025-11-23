c9a0bfb1538a8098ba4f34014a03b9d3a578893a
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-11-23 16:13:33 +0300

    hotfix(endpoint)

--- app/presentation/api/v1/router.py
+++ app/presentation/api/v1/router.py
@@ -3,8 +3,8 @@ from .endpoints import data, predict, dashboard, analytics, pile_history
 
 router = APIRouter(prefix="/api/v1")
 
-router.include_router(data.router, tags=["Data"])
+router.include_router(data.router, prefix="/data", tags=["Data"])
 router.include_router(predict.router, prefix="/predict", tags=["Prediction"])
 router.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
 router.include_router(analytics.router, prefix="/analytics", tags=["Analytics"])
-router.include_router(pile_history.router, prefix="", tags=["Pile History"])
+router.include_router(pile_history.router, prefix="/pile", tags=["Pile History"])
