6ebf50bc9b9813e5ba4e4467f515332ac057a7d3
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-11-25 11:11:22 +0300

    hotfix(front port): add port 88 to use nginx like revers proxy

--- app/main.py
+++ app/main.py
@@ -14,9 +14,10 @@ def create_app() -> FastAPI:
         allow_origins=[
             "http://localhost:3000",  # React
             "http://localhost:8501",  # Streamlit
-            "http://localhost:5174",  # Vite (ваш текущий фронтенд)
+            "http://localhost:5174",
             "http://178.208.85.7:8000",
             "http://localhost:5173",
+            "http://178.208.85.7:88",
             # разрешить все локальные порты:
             # "http://localhost:*",
         ],
