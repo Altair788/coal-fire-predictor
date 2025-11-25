de64628232efa4c540b0c3a8850e46d03764d12e
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-11-25 11:05:43 +0300

    hotfix(front port): add port to allow_origins

--- app/main.py
+++ app/main.py
@@ -15,7 +15,8 @@ def create_app() -> FastAPI:
             "http://localhost:3000",  # React
             "http://localhost:8501",  # Streamlit
             "http://localhost:5174",  # Vite (ваш текущий фронтенд)
-            "http://178.208.85.7:80",
+            "http://178.208.85.7:8000",
+            "http://localhost:5173",
             # разрешить все локальные порты:
             # "http://localhost:*",
         ],
